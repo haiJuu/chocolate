8da292de96a1f0f29db55e56b19479db4a9be07d
haijuu <xu3qo4b6820918@gmail.com>
2024-08-03 19:52:57 +0800

    update csv

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ChoColate</title>
     <!-- Css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="./bootstrap-5.2.3-dist/css/bootstrap.css"> -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.5.2/css/all.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -36,42 +35,47 @@
         <div id="nav" class="row">
             <nav class="navbar navbar-expand-lg bg-light ">
                 <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-                        aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul class="navbar-nav mx-auto">
 
                             <li class="nav-item ms-5 me-5 dropdown ">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    精選品牌
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    品牌介紹
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">起源ORIGIN</a></li>
-                                    <li><a class="dropdown-item" href="#">土庫驛TUKUYI</a></li>
+                                    <li><a class="dropdown-item" href="#">起源</a></li>
+                                    <li><a class="dropdown-item" href="#">土庫驛可可莊園</a></li>
                                     <li><a class="dropdown-item" href="#">好田家</a></li>
                                     <li><a class="dropdown-item" href="#">福灣巧克力</a></li>
                                 </ul>
                             </li>
 
-                            <li class="nav-item ms-5 me-5 dropdown ">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    商品訂購
+                            <li class="nav-item ms-5 me-5 dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    商品分類
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">黑巧克力</a></li>
+
+                                    <li class="nav-item dropend"><a class="dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">起源
+                                    </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">牛奶巧克力</a></li>
+                                        </ul>
+                                    </li>
+
                                     <li><a class="dropdown-item" href="#">牛奶巧克力</a></li>
+
                                     <li><a class="dropdown-item" href="#">白巧克力</a></li>
+
                                     <li><a class="dropdown-item" href="#">紅寶石巧克力</a></li>
-                                    <li><a class="dropdown-item" href="#">生巧克力</a></li>
+
                                 </ul>
                             </li>
                             <li class="nav-item ms-5 me-5">
-                                <a class="nav-link" href="#">可可知識</a>
+                                <a class="nav-link" href="#">最新消息</a>
                             </li>
                             <li class="nav-item ms-5 me-5">
                                 <a class="nav-link" href="#">關於我們</a>
@@ -87,14 +91,10 @@
     <section id="carousel">
         <div id="carouselExampleCaptions" class="carousel slide col-md-11 col-11" data-bs-ride="carousel">
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"
-                    aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -129,13 +129,11 @@
                     </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
@@ -278,9 +276,7 @@
     </footer>
 
     <!-- Js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <!-- <script src="./bootstrap-5.2.3-dist/js/bootstrap.bundle.js"></script> -->
 </body>
 
